4da96fe40e361d8e7df4207605972e55174e2fc5
Andrii Honcharuk <goncharukam@gmail.com>
2024-04-13 00:34:22 +0300

    hw2

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -1,14 +1,16 @@
 /*
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
-
-class Component {
-  constructor (public props:T) {
-
-  }
+interface IComponentProps {
+  title: string;
 }
 
-class Page extends Component {
+class Component <T> {
+  constructor(public props: T) {}
+}
+
+
+class Page extends Component <IComponentProps> {
   pageInfo () {
     console.log(this.props.title);
   }
